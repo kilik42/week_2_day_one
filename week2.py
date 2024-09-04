11255e47f83c600db957fb93f7cefe0124723dbf
marvin evins <marvinevins@yahoo.com>
2024-09-04 09:30:02 -0500

    first commit

---
+++ week2.py
@@ -0,0 +1,9 @@
+# ask someone what day it is
+day = input("What day is it? ")
+print("Today is", day)
+# ask them for their favorite holiday
+birthday = input("What is your favorite holiday? ")
+print("Your favorite holiday is", birthday)
+# print out the day and their favorite holiday in 
+# a sentence
+print("today is " + day + " and your favorite holiday is " + birthday)
